8129dc1dc3f426b95839b9cf8826fb0a6081810c
MadhanM282 <madhanm282@gmail.com>
2022-03-28 14:22:06 +0530

    first commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+
+export const Home = () => {
+  return (
+    <div className="homeContainer">
+      {[]
+        .filter((el) => { }) // Filter on the basis of Users interests and location (both true)
+        .map((el) => {
+          return (
+            <Link to={`add route here`} className="events">
+              {/* add your children here (divs)
+              ex : title, theme, description, date, time, location, image(optional)
+              the classNames should be also : title, theme, description, date, time, location, image(optional)
+
+                          {
+              "meetups": [
+                {
+                  "id": 1,
+                  "title": "Saunter Strech",
+                  "location": "bangalore",
+                  "date": "2022-03-25",
+                  "time": "23:59",
+                  "theme": "technology",
+                  "description": "Random Event",
+                  "image": "https://images.unsplash.com/photo"
+                }
+              ]
+            }
+             */}
+            </Link>
+          );
+        })}
+
+      <div className="subscribedData">
+        <div>
+          <select
+            value={"add your value here"}  // add value here
+            onChange={(e) => { }}
+          >
+            <option value="">------</option>
+            <option value="bangalore">Bangalore</option>
+            <option value="kolkata">Kolkata</option>
+            <option value="delhi">Delhi</option>
+            <option value="mumbai">Mumbai</option>
+          </select>
+        </div>
+        <Link to={`add your route here`}> Add Meetup</Link>
+        <h1>Subscribed Events</h1>
+        <div className="subscribedEvents">
+          {/* All user subcribed events should be displayed here in an ascending order of date */}
+
+          {[]
+            .map((el) => {
+              return (
+                <Link to={`add route here`} className="events">
+                  {/* Each event should have these elements/children (divs):
+                    ex : title, theme, description, date, time, location, image(optional)
+                    the classNames should be also : title, theme, description, date, time, location, image(optional) */}
+                </Link>
+              );
+            })}
+
+        </div>
+      </div>
+    </div>
+  );
+};
